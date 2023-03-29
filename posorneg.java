3a0733cb1dd7f1d5578e95b0a2e436dae47ed61c
Nancy01m <129317245+Nancy01m@users.noreply.github.com>
2023-03-29 23:48:06 +0530

    Add files via upload

---
+++ posorneg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class posorneg{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+        return;
+    }
+}
