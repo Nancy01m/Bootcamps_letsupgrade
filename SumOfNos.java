edb81413c608c4edab726ee58ca42cbdf35827ab
Nancy01m <129317245+Nancy01m@users.noreply.github.com>
2023-03-31 22:41:36 +0530

    Add files via upload
    java bootcamp DAY-5 assignment

---
+++ SumOfNos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class SumOfNos{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=num;i++){
+			sum=sum+i;
+		}
+		System.out.println("The sum of all integers from 1 to "+num+" is "+sum);
+	}
+}
+		
